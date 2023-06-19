898c54b87fa9a532fb2477f9dbfc8c6b266aca8e
sushantshekhar-tech <116714886+sushantshekhar-tech@users.noreply.github.com>
2023-06-19 21:10:43 +0530

    Add files via upload

---
+++ LED_Controlled_buttons_using_ESP8266_webserver/data/script.js
@@ -0,0 +1,161 @@
+window.addEventListener('load', getStates);
+// Function to get and update GPIO states on the web page
+function getStates(){
+ var xhr = new XMLHttpRequest();
+ xhr.onreadystatechange = function() {
+ if (this.readyState == 4 && this.status == 200) {
+ var myObj = JSON.parse(this.responseText);
+ console.log(myObj);
+ for (i in myObj.gpios){
+ var output = myObj.gpios[i].output;
+ var state = myObj.gpios[i].state;
+ console.log(output);
+ console.log(state);
+ if (state == "1") {
+
+ document.getElementById(output).checked = true;
+ document.getElementById(output+"s").innerHTML = "ON";
+ }
+ else {
+ document.getElementById(output).checked = false;
+ document.getElementById(output+"s").innerHTML = "OFF";
+ }
+ }
+ }
+ };
+ xhr.open("GET", "/states", true);
+ xhr.send();
+}
+// Send Requests to Control GPIOs
+function toggleCheckbox (element) {
+ var xhr = new XMLHttpRequest();
+ if (element.checked) {
+ xhr.open("GET", "/update?output="+element.id+"&state=1", true);
+ document.getElementById(element.id+"s").innerHTML = "ON";
+ }
+ else {
+ xhr.open("GET", "/update?output="+element.id+"&state=0", true);
+ document.getElementById(element.id+"s").innerHTML = "OFF";
+ }
+ xhr.send();
+}window.addEventListener('load', getStates);
+// Function to get and update GPIO states on the web page
+function getStates(){
+ var xhr = new XMLHttpRequest();
+ xhr.onreadystatechange = function() {
+ if (this.readyState == 4 && this.status == 200) {
+ var myObj = JSON.parse(this.responseText);
+ console.log(myObj);
+ for (i in myObj.gpios){
+ var output = myObj.gpios[i].output;
+ var state = myObj.gpios[i].state;
+ console.log(output);
+ console.log(state);
+ if (state == "1") {
+
+ document.getElementById(output).checked = true;
+ document.getElementById(output+"s").innerHTML = "ON";
+ }
+ else {
+ document.getElementById(output).checked = false;
+ document.getElementById(output+"s").innerHTML = "OFF";
+ }
+ }
+ }
+ };
+ xhr.open("GET", "/states", true);
+ xhr.send();
+}
+// Send Requests to Control GPIOs
+function toggleCheckbox (element) {
+ var xhr = new XMLHttpRequest();
+ if (element.checked) {
+ xhr.open("GET", "/update?output="+element.id+"&state=1", true);
+ document.getElementById(element.id+"s").innerHTML = "ON";
+ }
+ else {
+ xhr.open("GET", "/update?output="+element.id+"&state=0", true);
+ document.getElementById(element.id+"s").innerHTML = "OFF";
+ }
+ xhr.send();
+}window.addEventListener('load', getStates);
+// Function to get and update GPIO states on the web page
+function getStates(){
+ var xhr = new XMLHttpRequest();
+ xhr.onreadystatechange = function() {
+ if (this.readyState == 4 && this.status == 200) {
+ var myObj = JSON.parse(this.responseText);
+ console.log(myObj);
+ for (i in myObj.gpios){
+ var output = myObj.gpios[i].output;
+ var state = myObj.gpios[i].state;
+ console.log(output);
+ console.log(state);
+ if (state == "1") {
+
+ document.getElementById(output).checked = true;
+ document.getElementById(output+"s").innerHTML = "ON";
+ }
+ else {
+ document.getElementById(output).checked = false;
+ document.getElementById(output+"s").innerHTML = "OFF";
+ }
+ }
+ }
+ };
+ xhr.open("GET", "/states", true);
+ xhr.send();
+}
+// Send Requests to Control GPIOs
+function toggleCheckbox (element) {
+ var xhr = new XMLHttpRequest();
+ if (element.checked) {
+ xhr.open("GET", "/update?output="+element.id+"&state=1", true);
+ document.getElementById(element.id+"s").innerHTML = "ON";
+ }
+ else {
+ xhr.open("GET", "/update?output="+element.id+"&state=0", true);
+ document.getElementById(element.id+"s").innerHTML = "OFF";
+ }
+ xhr.send();
+}window.addEventListener('load', getStates);
+// Function to get and update GPIO states on the web page
+function getStates(){
+ var xhr = new XMLHttpRequest();
+ xhr.onreadystatechange = function() {
+ if (this.readyState == 4 && this.status == 200) {
+ var myObj = JSON.parse(this.responseText);
+ console.log(myObj);
+ for (i in myObj.gpios){
+ var output = myObj.gpios[i].output;
+ var state = myObj.gpios[i].state;
+ console.log(output);
+ console.log(state);
+ if (state == "1") {
+
+ document.getElementById(output).checked = true;
+ document.getElementById(output+"s").innerHTML = "ON";
+ }
+ else {
+ document.getElementById(output).checked = false;
+ document.getElementById(output+"s").innerHTML = "OFF";
+ }
+ }
+ }
+ };
+ xhr.open("GET", "/states", true);
+ xhr.send();
+}
+// Send Requests to Control GPIOs
+function toggleCheckbox (element) {
+ var xhr = new XMLHttpRequest();
+ if (element.checked) {
+ xhr.open("GET", "/update?output="+element.id+"&state=1", true);
+ document.getElementById(element.id+"s").innerHTML = "ON";
+ }
+ else {
+ xhr.open("GET", "/update?output="+element.id+"&state=0", true);
+ document.getElementById(element.id+"s").innerHTML = "OFF";
+ }
+ xhr.send();
+}
---
+++ LED_Controlled_buttons_using_ESP8266_webserver/src/main.cpp
@@ -0,0 +1,99 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
+#include <Arduino_JSON.h>
+// Replace with your network credentials
+
+const char* ssid = "Nayan 4G";
+const char* password = "Babu@1234";
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+//Input Parameters
+const char* PARAM_INPUT_OUTPUT = "output";
+const char* PARAM_INPUT_STATE = "state";
+// Set number of outputs
+#define NUM_OUTPUTS 2
+// Array with outputs you want to control
+int outputGPIOs[NUM_OUTPUTS] = {2, 4};
+// Initialize LittleFS
+void initFS() {
+ if (!LittleFS.begin()) {
+ Serial.println("An error has occurred while mounting LittleFS");
+ }
+ Serial.println("LittleFS mounted successfully");
+}
+// Initialize WiFi
+void initWiFi() {
+ WiFi.mode(WIFI_STA);
+ WiFi.begin(ssid, password);
+ Serial.print("Connecting to WiFi ..");
+ while (WiFi.status() != WL_CONNECTED) {
+ Serial.print('.');
+ delay(1000);
+ }
+ Serial.println(WiFi.localIP());
+}
+// Return JSON with Current Output States
+String getOutputStates(){
+ JSONVar myArray;
+ for (int i =0; i<NUM_OUTPUTS; i++){
+ myArray["gpios"][i]["output"] = String(outputGPIOs[i]);
+ myArray["gpios"][i]["state"] = String(digitalRead(outputGPIOs[i]));
+ //myArray["gpios"][i] ="{\"output\":\"" + String(outputGPIOs[i]) + "\", \"sta
+// te\": \"" + String(digitalRead(outputGPIOs[i])) + "\"}";
+ }
+ String jsonString = JSON.stringify(myArray);
+ Serial.print(jsonString);
+ return jsonString;
+}
+void setup(){
+ // Serial port for debugging purposes
+ Serial.begin(115200);
+ // Set GPIOs as outputs
+
+ for (int i =0; i<NUM_OUTPUTS; i++){
+ pinMode(outputGPIOs[i], OUTPUT);
+ }
+ initWiFi();
+ initFS();
+ 
+ // Route for root / web page
+ server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+ request->send(LittleFS, "/index.html", "text/html",false);
+ });
+ server.serveStatic("/", LittleFS, "/");
+ server.on("/states", HTTP_GET, [](AsyncWebServerRequest *request){
+ String json = getOutputStates();
+ request->send(200, "application/json", json);
+ json = String();
+ });
+ //GET request to <ESP_IP>/update?output=<output>&state=<state>
+ server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+ String output;
+ String state;
+ // GET input1 value on <ESP_IP>/update?output=<output>&state=<state>
+ if (request->hasParam(PARAM_INPUT_OUTPUT) && request->hasParam(PARAM_INPUT_STATE))
+ {
+ output = request->getParam(PARAM_INPUT_OUTPUT)->value();
+ state = request->getParam(PARAM_INPUT_STATE)->value();
+ // Control GPIO
+ digitalWrite(output.toInt(), state.toInt());
+ }
+ else {
+ output = "No message sent";
+ state = "No message sent";
+ }
+ Serial.print("GPIO: ");
+ Serial.print(output);
+ Serial.print(" - Set to: ");
+ Serial.println(state);
+ 
+ request->send(200, "text/plain", "OK");
+ });
+ // Start server
+ server.begin();
+}
+void loop() {
+}
